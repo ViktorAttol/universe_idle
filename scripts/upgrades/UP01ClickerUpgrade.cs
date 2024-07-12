cb2c44d68154f92052ac968a7a65d2a791656561
ViktorAttol <j.kolesnyk@gmx.de>
2024-07-12 22:03:09 +0200

    Starting on upgrades.

---
+++ scripts/upgrades/UP01ClickerUpgrade.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+/// <Summary>
+/// Upgrade 01 - Increases stardust created by the clicker
+/// </Summary>
+public partial class UP01ClickerUpgrade : Node
+{
+	public int level = 0;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
